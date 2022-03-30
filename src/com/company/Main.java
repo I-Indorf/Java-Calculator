929224acbc8b127ef73802af0bb95411ffa4667d
I-Indorf <irene.indorf@hotmail.com>
2022-03-30 12:21:56 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("add 1 + 3: %d\n", Calculator.add(1, 3));
+        System.out.printf("subtract 1 - 3: %d\n", Calculator.subtract(1, 3));
+        System.out.printf("multiply 2 * 7: %d\n", Calculator.multiply(2, 7));
+        System.out.printf("divide 10.0 / 3.0: %f\n", Calculator.divide(10, 3));
+        System.out.printf("square 4: %d\n", Calculator.square(4));
+
+        char piSymbol = '\u03C0';
+
+    }
+
+}
